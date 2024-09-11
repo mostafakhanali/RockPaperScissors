253a7e48ef6707b17193c3a350433839de9f80ed
Mostafa <mostafakhanalipoor@gmail.com>
2024-09-11 16:28:24 +0330

    Creating UI for app and change the app script. - The UI components added in index.html. - Styles added to new style.css. - The app script file in was script.js was changed   to make the GUI interactive based on user actions. - New font's added to app.

--- script.js
+++ script.js
@@ -1,59 +1,108 @@
+const CHOICE = ['✊🏼', '✋🏼', '✌🏼'];
+const choice = document.querySelector('.choice');
+const player = document.querySelector('.user--player');
+const computer = document.querySelector('.user--com');
+const winModal = document.querySelector('dialog');
+const winnerText = document.querySelector('dialog div');
+const playAgain = document.querySelector('dialog button');
+const state = document.querySelector('.state');
 
-const CHOICE = ['rock', 'paper', 'scissors'];
 let humanScore = 0;
 let computerScore = 0;
+let round = 0;
+let wait2sec;
+let currentChoice;
+
+const resetCards = function () {
+    currentChoice?.classList.remove('selected');
+    player.querySelector('.choose').textContent = '?';
+    computer.querySelector('.choose').textContent = '?';
+    player.querySelector('.choose').classList.remove('active', 'win');
+    computer.querySelector('.choose').classList.remove('active', 'win');
+};
+const reset = function () {
+    humanScore = 0;
+    computerScore = 0;
+    round = 0;
+    updateScore(player.querySelector('.score'), humanScore);
+    updateScore(computer.querySelector('.score'), computerScore);
+    resetCards();
+    winModal.close();
+};
 
 const getComputerChoice = function () {
-    const computerChoice = CHOICE[Math.trunc(Math.random() * 3)]
-    return computerChoice
-}
+    const computerChoice = CHOICE[Math.trunc(Math.random() * 3)];
+    return computerChoice;
+};
 
-const getHumanChoice = function () {
-    let selection;
-    while (!(selection && typeof selection === 'number' && selection > 0 && selection < 4)) {
-        selection = +prompt(`Select:
-            1: Rock
-            2: paper
-            3: Scissors`);
+choice.addEventListener('click', function (e) {
+    currentChoice?.classList.remove('selected');
+    currentChoice = e.target;
+    const weapon = e.target.textContent;
+    if (round < 5 && round >= 0) {
+        clearTimeout(wait2sec);
+        round++;
+        currentChoice.classList.add('selected');
+        player.querySelector('.choose').classList.remove('win');
+        computer.querySelector('.choose').classList.remove('win');
+        const comWeapon = getComputerChoice();
+        player.querySelector('.choose').textContent = weapon;
+        computer.querySelector('.choose').textContent = comWeapon;
+        player.querySelector('.choose').classList.add('active');
+        computer.querySelector('.choose').classList.add('active');
+        playRound(weapon, comWeapon);
+
+        wait2sec = setTimeout(resetCards, 2000);
+        if (round === 5) {
+            setTimeout(function () {
+                if (humanScore > computerScore) {
+                    winnerText.innerHTML = ` <p>🔥🔥🔥🔥</p>
+                                                <p>
+                                                You Win 🏆
+                                                </p>
+                                                <p>${humanScore}  - ${computerScore}<\p>
+                                                <p>🔥🔥🔥🔥</p>`;
+                    winModal.style.backgroundColor = 'var(--color-lime-5)';
+                } else if (computerScore > humanScore) {
+                    winnerText.innerHTML = `<p>You lose 😞</p>
+                                            <p>${humanScore} - ${computerScore}<\p>`;
+                    winModal.style.backgroundColor = 'var(--color-red-6)';
+                } else {
+                    winnerText.innerHTML = `<p>Draw 😐</p>
+                    <p>${humanScore} - ${computerScore}<\p>`;
+                    winModal.style.backgroundColor = 'var(--color-teal-3)';
+                }
+                winModal.showModal();
+            }, 1500);
+        }
     }
-    const HumanChoice = CHOICE[selection - 1]
-    return HumanChoice
-}
+});
 
 const playRound = function (humanChoice, computerChoice) {
     if (CHOICE.includes(humanChoice) && CHOICE.includes(computerChoice)) {
         if (humanChoice == computerChoice) {
-            return `Draw! ${humanChoice} Vs. ${computerChoice}`
-        }
-        else if
-            (
-            (humanChoice == 'rock' && computerChoice == 'scissors') ||
-            (humanChoice == 'scissors' && computerChoice == 'paper') ||
-            (humanChoice == 'paper' && computerChoice == 'rock')
+            state.textContent = 'Draw';
+            return `Draw! ${humanChoice} Vs. ${computerChoice}`;
+        } else if (
+            (humanChoice == '✊🏼' && computerChoice == '✌🏼') ||
+            (humanChoice == '✌🏼' && computerChoice == '✋🏼') ||
+            (humanChoice == '✋🏼' && computerChoice == '✊🏼')
         ) {
             humanScore++;
-            return `You win! ${humanChoice} Beats ${computerChoice}`
-        }
-        else {
+            player.querySelector('.choose').classList.add('win');
+            state.textContent = 'You win';
+            updateScore(player.querySelector('.score'), humanScore);
+        } else {
             computerScore++;
-            return `You lose! ${computerChoice} Beats ${humanChoice}`
+            computer.querySelector('.choose').classList.add('win');
+            state.textContent = 'You lose';
+            updateScore(computer.querySelector('.score'), computerScore);
         }
     }
-}
+};
 
-const playGame = function () {
-    for (let i = 0; i < 5; i++) {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        let result = playRound(humanSelection, computerSelection);
-        console.log(result);
-    }
-    if (humanScore > computerScore) {
-        console.log(`you win 🏆\nyour score: ${humanScore}\ncomputer score: ${computerScore}`);
-    }
-    else {
-        console.log(`you lose 🔴\nyour score: ${humanScore}\ncomputer score: ${computerScore}`);
-    }
-}
+const updateScore = function (user, score) {
+    user.textContent = `Score: ${score}/5`;
+};
 
-playGame()
+playAgain.addEventListener('click', reset);
